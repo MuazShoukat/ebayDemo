2289a8462885154d9dbe084a6cef74886c592bdf
MuazShoukat <107341142+MuazShoukat@users.noreply.github.com>
2022-06-15 15:16:26 +0500

    Update credentials

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array(
       "rR+H7inckBN7cVr3cXM9Gg==,eNmi21neJDAD8lsEObMLQg==,RY/ZjJJNUujydumQDMbcIQ==,oSRX3F89h/JHaCI/LbmyJw=="
